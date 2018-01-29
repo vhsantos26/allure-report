47ac2b4e965c3da22a334b9f3b185ee2fdfc98ff
Victor Hugo dos Santos <vhsantos26@gmail.com>
2018-01-29 10:52:58 -0200

    ajustes para trend

--- generate-report.sh
+++ generate-report.sh
@@ -1,4 +1,8 @@
 #!/bin/bash +x
 
+cp -a categories.json allure-results/
+
+cp -a allure-report/history/ allure-results/
+
 echo "----|---- INFO: GERANDO RELATORIOS"
 allure generate allure-results/ --clean
